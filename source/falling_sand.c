5ce0e9ba2e1b2eb8d449e3ceec3c11a225ee52c6
wrac4242 <42539664+wrac4242@users.noreply.github.com>
2021-01-16 17:03:45 +0000

    creates empty window, of set size

--- source/falling_sand.c
+++ source/falling_sand.c
@@ -1,13 +1,42 @@
-#include <stdio.h>
+#include <stdio.h> //input and output for debugging
+#include <SDL.h> //sdl2 library
+
 
 //screen settings, can be changed later
-int screen_width = 110;
-int screen_height = 110;
-int screen_boarder_size = 10;
+const int SCREEN_WIDTH = 110;
+const int SCREEN_HEIGHT = 110;
 
-
-int main()
+int main(int argc, char* args[])
 {
-  printf("hello world\n");
-  printf("test setup!!");
+  SDL_Window *window;                    // Declare a pointer
+
+    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
+
+    // Create an application window with the following settings:
+    window = SDL_CreateWindow(
+        "falling_sand",                    // window title
+        SDL_WINDOWPOS_UNDEFINED,           // initial x position
+        SDL_WINDOWPOS_UNDEFINED,           // initial y position
+        SCREEN_WIDTH,                      // width, in pixels
+        SCREEN_HEIGHT,                     // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    // Check that the window was successfully created
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        printf("Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // The window is open: could enter program loop here (see SDL_PollEvent())
+
+    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+    // Close and destroy the window
+    SDL_DestroyWindow(window);
+
+    // Clean up
+    SDL_Quit();
+    return 0;
 }
