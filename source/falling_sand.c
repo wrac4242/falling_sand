1332988f3cfae771fb35f917e643b008fb095999
wrac4242 <42539664+wrac4242@users.noreply.github.com>
2021-01-17 18:42:17 +0000

    added array initilisation, added way to create more materials

--- source/falling_sand.c
+++ source/falling_sand.c
@@ -4,32 +4,37 @@
 
 //function prototypes
 int initialise_window(); //initialises the game screen, returns 0 if successful
-int exit_game(long window_addr); //exits the game, returns 0 if successful
+int exit_game(long window_addr, int* array); //exits the game, returns 0 if successful
+
+int * createArray(int array_x, int array_y); //creates array of given size of particles, returns array pointer
 
 //structs & enums
 typedef enum {false, true} bool;
 
 //colour struct, 32 bits, standard pixel data
-typedef struct colour_t
-{
+typedef struct colour_t{
   __u8 r;
   __u8 g;
   __u8 b;
   __u8 a;
-} colour_t;
+}colour_t;
 
-typedef struct particle_t
-{
+typedef struct particle_t{
   __u32 id; //4 bytes, material id
   __u32 life_time; //4 bytes, only used for limited life particles
   colour_t colour; //4 bytes
   __u32 lastUpdatedFrame; //4 bytes
 } particle_t; //16 bytes
 
+// material id, for particle info
+#define mat_id_empty (__u8)0
+
+// colours
+#define mat_col_empty (colour_t){ 0, 0, 0, 0}
 
 //screen settings, can be changed later
-const int SCREEN_WIDTH = 110;
-const int SCREEN_HEIGHT = 110;
+const int SCREEN_WIDTH = 128;
+const int SCREEN_HEIGHT = 128;
 __u32 frameNumber = 0; //frame number
 
 int main(int argc, char* args[])
@@ -40,6 +45,7 @@ int main(int argc, char* args[])
 
   //initial creation of assets needed
   //create the array of particles
+  int* particle_array = createArray(123,321); //pointer to the array in memory
 
   bool playing = true; //creates the playing variable
 
@@ -54,11 +60,14 @@ int main(int argc, char* args[])
     if(event.type == SDL_QUIT){
       playing = false;
       printf("exiting via quit event\n");
+
+      //printf("Number of rows: %d\n", ((int) (sizeof (particle_array) / sizeof (particle_array)[0])));
+      //printf("Number of columns: %d\n", ((int) (sizeof (particle_array[0]) / sizeof (particle_array[0])[0])));
     }
   }
   while (playing==1);
 
-  int error_code_exit = exit_game((long) &window);
+  int error_code_exit = exit_game((long) &window, particle_array);
   if (error_code_exit!=0){
     printf("Error in game exit\n");
     return 1;
@@ -90,7 +99,7 @@ int initialise_window() //initialises the game screen, returns 0 if successful
   }
 }
 
-int exit_game(long window_addr) //exits the game, returns 0 if successful
+int exit_game(long window_addr, int* array) //exits the game, returns 0 if successful
 {
   SDL_Window *window;
   window = (void*) (long) window_addr;
@@ -99,5 +108,22 @@ int exit_game(long window_addr) //exits the game, returns 0 if successful
   SDL_DestroyWindow(window);
 
   SDL_Quit();
+
+  free(array);
+
   return 0;
+}
+
+int * createArray(int array_x, int array_y)
+{
+  particle_t (*array)[array_x] = (int (*)[array_y])malloc(array_x * array_y*sizeof(particle_t)); //creates array
+
+  for (int i = 0; i < array_x; i++) {
+    for (int j = 0; j < array_y; j++) {
+      struct particle_t p; p.id=mat_id_empty, p.life_time=0, p.colour=mat_col_empty, p.lastUpdatedFrame=666; //random number chosen for last updated frame no difference
+      array[i][j] = p;
+    }
+  }
+
+  return array;
 }
